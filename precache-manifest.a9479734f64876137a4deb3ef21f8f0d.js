2ef44cfd609954cd112d85aa69a870c9faadb604
riddhi_13 <riddhi.busha99@gmail.com>
2020-06-10 11:22:32 +0530

    Updates

---
+++ precache-manifest.a9479734f64876137a4deb3ef21f8f0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9af8449099d8600fcabd8d594cef07c",
+    "url": "/diagnal-app/index.html"
+  },
+  {
+    "revision": "b53b5a5587f2ffdb7993",
+    "url": "/diagnal-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fffb5d5cc1a23eb38c5b",
+    "url": "/diagnal-app/static/js/2.6d94dd4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diagnal-app/static/js/2.6d94dd4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53b5a5587f2ffdb7993",
+    "url": "/diagnal-app/static/js/main.8f54c9ca.chunk.js"
+  },
+  {
+    "revision": "1ab1fe689d2e4ed70b42",
+    "url": "/diagnal-app/static/js/runtime-main.614e790e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/diagnal-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
